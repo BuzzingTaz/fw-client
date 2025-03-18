b41511c7a03f5567ccebcd789f12ae97b3996581
BuzzingTaz <catzfgnam@gmail.com>
2025-03-18 18:02:21 +0530

    Added minimal camera setup, tasks selection with constraints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,168 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { startCamera } from "@/core/sensors/camera";
+import { getTaskConstraints, Tasks } from "@/app/lib/tasks";
+
+//const taskList = [{ id: "task1", title: "task1" }, { id: "task2", title: "task2" }];
+// const taskList = Object.keys(Tasks).map((task) => ({ id: task, title: task }));
+const taskList = Object.keys(Tasks);
+const resolutionList = ["640x480", "1280x720", "1600x900", "1920x1080"];
+const fpsList = [10, 30, 60];
+const netMethodList = ["webrtc", "ws", "http"];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const VideoRef = useRef<HTMLVideoElement>(null);
+  const appRunning = useRef(false);
+  const [selectedTask, setSelectedTask] = useState<string>(taskList[0]);
+  const [selectedResolution, setSelectedResolution] = useState<string>(
+    resolutionList[0],
+  );
+  const [selectedFps, setSelectedFps] = useState<number>(fpsList[0]);
+  const [selectedNetMethod, setSelectedNetMethod] = useState<string>(
+    netMethodList[0],
+  );
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const setVideoRef = async (
+    VideoRef: React.RefObject<HTMLVideoElement | null>,
+    width: number,
+    height: number,
+    fps: number,
+  ) => {
+    console.log(width, height);
+    if (VideoRef.current && VideoRef.current.srcObject) {
+      console.log("Stopping stream");
+      const stream = VideoRef.current.srcObject as MediaStream;
+      stream.getTracks().forEach((track) => track.stop());
+    }
+
+    const mediaStream = await startCamera(width, height, fps);
+
+    if (VideoRef.current) {
+      console.log("Refreshing Camera");
+      VideoRef.current.srcObject = mediaStream;
+    }
+  };
+
+  const onStart = async () => {
+    const supportedConstraints =
+      navigator.mediaDevices.getSupportedConstraints();
+    console.log(supportedConstraints);
+    console.log(selectedTask);
+    const taskConstraints = getTaskConstraints(selectedTask);
+
+    const resolutionHeight = taskConstraints.video.height;
+    const resolutionWidth = taskConstraints.video.width;
+    const fps = taskConstraints.video.frameRate;
+
+    await setVideoRef(VideoRef, resolutionWidth, resolutionHeight, fps);
+
+    appRunning.current = true;
+  };
+
+  useEffect(() => {
+    if (!appRunning.current) return;
+    const width = Number(selectedResolution.split("x")[0]);
+    const height = Number(selectedResolution.split("x")[1]);
+    setVideoRef(VideoRef, width, height, selectedFps);
+  }, [selectedResolution, selectedFps]);
+
+  useEffect(() => {
+    if (!appRunning.current) return;
+    const taskConstraints = getTaskConstraints(selectedTask);
+    const height = taskConstraints.video.height;
+    const width = taskConstraints.video.width;
+    const fps = taskConstraints.video.frameRate;
+    setVideoRef(VideoRef, width, height, fps);
+  }, [selectedTask]);
+
+  return (
+    <div>
+      <video className="h-1/2 w-1/2" ref={VideoRef} autoPlay playsInline />
+      <div className="flex flex-col">
+        <div>
+          <label>Task</label>
+          <select
+            className="border border-gray-900 rounded-md p-2 bg-gray-100"
+            name="task Select"
+            id="taskSelect"
+            value={selectedTask}
+            onChange={async (e) => {
+              setSelectedTask(e.target.value);
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {taskList.map((task) => (
+              <option key={task} value={task}>
+                {task}
+              </option>
+            ))}
+          </select>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div>
+          <label>Resolution</label>
+          <select
+            className="border border-gray-900 rounded-md p-2 bg-gray-100"
+            name="resolution Select"
+            id="resolutionSelect"
+            value={selectedResolution}
+            onChange={async (e) => {
+              setSelectedResolution(e.target.value);
+            }}
+          >
+            {resolutionList.map((resolution) => (
+              <option key={resolution} value={resolution}>
+                {resolution}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label>FPS</label>
+          <select
+            className="border border-gray-900 rounded-md p-2 bg-gray-100"
+            name="fps Select"
+            id="fpsSelect"
+            onChange={async (e) => {
+              setSelectedFps(Number(e.target.value));
+            }}
+          >
+            {fpsList.map((fps) => (
+              <option key={fps} value={fps}>
+                {fps}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label>Network Method</label>
+          <select
+            className="border border-gray-900 rounded-md p-2 bg-gray-100"
+            name="netMethod Select"
+            id="netMethodSelect"
+            value={selectedNetMethod}
+            onChange={async (e) => {
+              setSelectedNetMethod(e.target.value);
+            }}
+          >
+            {netMethodList.map((netMethod) => (
+              <option key={netMethod} value={netMethod}>
+                {netMethod}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      <button
+        className="border border-gray-900 rounded-md p-2 bg-gray-100"
+        onClick={async () => {
+          await onStart();
+        }}
+      >
+        Start
+      </button>
     </div>
   );
 }
