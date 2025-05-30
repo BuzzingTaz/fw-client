d9253d18428c87d61782784442bef30a7bd08e16
BuzzingTaz <catzfgnam@gmail.com>
2025-05-30 19:42:56 +0530

    Add reusable dropdown components that make use of tasks info

---
+++ src/app/ui/components/ResolutionDropdown.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { Task, VideoResolution } from "@/app/lib/definitions";
+import { useQuery } from "@tanstack/react-query";
+
+interface ResolutionDropdownProps {
+  taskID: string;
+  selectedResolution: VideoResolution | null;
+  onChange: (resolution: VideoResolution) => void;
+}
+
+const ResolutionDropdown: React.FC<ResolutionDropdownProps> = ({
+  taskID,
+  selectedResolution,
+  onChange,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selected = resolutionList.find(
+      (res) => `${res.width}x${res.height}` === e.target.value,
+    );
+    if (selected) {
+      onChange(selected);
+    }
+  };
+
+  const {
+    data: tasks,
+    isLoading,
+    isError,
+  } = useQuery<Task[]>({
+    queryKey: [taskID],
+    queryFn: () => fetch(`/api/tasks`).then((res) => res.json()),
+  });
+
+  if (isLoading) {
+    return <div>Loading resolution options...</div>;
+  }
+  if (isError) {
+    return <div>Error loading resolution options</div>;
+  }
+
+  const resolutionList = tasks!.find((task) => task.taskID === taskID)
+    ?.constraints.resolutions;
+
+  return (
+    <select
+      value={
+        selectedResolution
+          ? `${selectedResolution.width}x${selectedResolution.height}`
+          : ""
+      }
+      onChange={handleChange}
+      className="border border-gray-900 rounded-md p-2 bg-gray-100"
+    >
+      {resolutionList!.map((res) => (
+        <option
+          key={`${res.width}x${res.height}`}
+          value={`${res.width}x${res.height}`}
+        >
+          {res.label || `${res.width}x${res.height} (${res.aspectRatio})`}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ResolutionDropdown;
---
+++ src/app/ui/components/Sidebar.tsx
@@ -0,0 +1,54 @@
+"use client";
+// TODO: Make this a server component
+import Link from "next/link";
+import { Task } from "@/app/lib/definitions";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Sidebar() {
+  const {
+    data: tasks,
+    isLoading,
+    isError,
+  } = useQuery<Task[]>({
+    queryKey: ["sidebar"],
+    queryFn: () => fetch(`/api/tasks`).then((res) => res.json()),
+  });
+
+  if (isLoading) {
+    return <div>Loading tasks...</div>;
+  }
+  if (isError) {
+    return <div>Error loading tasks</div>;
+  }
+
+  return (
+    <div className="w-64 h-screen bg-gray-800 text-white p-4 flex flex-col">
+      <Link
+        href="/"
+        className="flex items-center space-x-2 p-3 rounded-lg hover:bg-gray-700
+        mb-4"
+      >
+        {/*TODO: ICON*/}
+        <span>Home</span>
+      </Link>
+
+      {/* Tasks Section */}
+      <div className="flex-1 overflow-y-auto">
+        <h3 className="text-xs uppercase tracking-wider text-gray-400 mb-2 px-3">
+          Tasks
+        </h3>
+        <div className="space-y-1">
+          {tasks.map((task) => (
+            <Link
+              key={task.taskID}
+              href={`/${task.taskID}`}
+              className="w-full text-left p-3 rounded-lg hover:bg-gray-700 flex items-center"
+            >
+              <span className="truncate">{task.name}</span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
