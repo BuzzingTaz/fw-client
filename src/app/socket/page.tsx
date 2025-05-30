075542ee89eaa2b2a6f112e997fdedd3d8c42c02
BuzzingTaz <catzfgnam@gmail.com>
2025-05-30 19:36:23 +0530

    made a reusable socket hook

--- src/app/socket/page.tsx
+++ src/app/socket/page.tsx
@@ -1,56 +1,14 @@
 "use client";
+import { useSocket } from "@/app/lib/hooks/useSocket";
 
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-
-// Connect to existing socketio at ws://localhost:8080
-const socket = io("http://localhost:8080");
-
+// Socket test page
+// TODO: Remove after finalizing socket
 export default function Page() {
-  const [socketData, setSocketData] = useState<string>("");
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState("N/A");
-
-  useEffect(() => {
-    if (socket.connected) {
-      onConnect();
-    }
-
-    function onConnect() {
-      setIsConnected(true);
-      setTransport(socket.io.engine.transport.name);
-
-      socket.io.engine.on("upgrade", (transport) => {
-        setTransport(transport.name);
-      });
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-      setTransport("N/A");
-    }
-
-    function onHello(data: string) {
-      setSocketData(data);
-      console.log(data);
-    }
-
-    socket.emit("hello", "world");
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-    socket.on("hello", onHello);
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.off("hello", onHello);
-    };
-  }, []);
+  const { isConnected, transport } = useSocket();
 
   return (
     <div>
       <h1>Socket Page</h1>
-      <p>Socket Data: {socketData}</p>
       <p>Connected: {isConnected.toString()}</p>
       <p>Transport: {transport}</p>
     </div>
